a66d384e8e0eb5f5013b2a323e8ebe04e873acb9
pabodiniudari <108340204+pabodiniudari@users.noreply.github.com>
2023-10-15 18:31:22 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+git checkout -b feature-branch
+  def hello(name):
+   print (f("Hello, {name}!")
+  if__name__ = "__main__":
+   name = input("Enter your name:")
+    hello(name)
+git add.
+git commit -m "hello.py in feature-branch"
+git checkout master
+git merge feature-branch
+git push origin master
